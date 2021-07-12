73051bcc4807be52ea926aaf2c6464a577c0f75c
SM1701 <717513736@qq.com>
2021-07-12 11:41:43 +0800

    define const

--- ch1/main.c
+++ ch1/main.c
@@ -4,6 +4,10 @@
 
 #include "stdio.h"
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 void hello();
 
 void temp();
@@ -12,11 +16,14 @@ void tempFloat();
 
 void p1_5();
 
+void tempConst();
+
 int main() {
     hello();
     temp();
     tempFloat();
     p1_5();
+    tempConst();
     return 0;
 }
 
@@ -35,11 +42,13 @@ void temp() {
     step = 20;
 
     fahr = lower;
+    printf("temp begin==================>\n");
     while (fahr <= upper) {
         celsius = 5 * (fahr - 32) / 9;
         printf("%d\t%d\n", fahr, celsius);
         fahr = fahr + step;
     }
+    printf("<================== temp end\n");
 
 }
 
@@ -52,11 +61,13 @@ void tempFloat() {
     step = 20;
 
     fahr = lower;
+    printf("tempFloat begin==================>\n");
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%0.3f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+    printf("<================== tempFloat end\n");
 }
 
 void p1_5() {
@@ -68,9 +79,18 @@ void p1_5() {
     step = 20;
 
     fahr = upper;
+    printf("p1_5 begin==================>\n");
     while (fahr >= lower) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%0.3f %6.1f\n", fahr, celsius);
         fahr = fahr - step;
+    }
+    printf("<================== p1_5 end\n");
+}
+
+void tempConst() {
+    int fahr;
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
 }
