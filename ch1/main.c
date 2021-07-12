60f1847ecb91d2579369fd147a58b93e309397a9
SM1701 <717513736@qq.com>
2021-07-12 17:09:57 +0800

    add todo for extern

--- ch1/main.c
+++ ch1/main.c
@@ -47,6 +47,17 @@ int get_line(char s[], int lim);
 
 void copy(char to[], const char from[]);
 
+void longest_line_ex();
+
+int get_line_ex(void);
+
+void copy_ex(void);
+
+
+/**
+ * TODO 做成命令行工具
+ * @return
+ */
 int main() {
 //    hello();
 //    temp();
@@ -62,7 +73,8 @@ int main() {
 //    count_words();
 //    count_digits();
 //    test_power();
-    longest_line();
+//    longest_line();
+    longest_line_ex();
 
     return 0;
 }
@@ -342,4 +354,52 @@ int get_line(char s[], int lim) {
 
     s[i] = '\0';
     return i;
+}
+
+
+/**
+ * 编译失败
+ * TODO
+ * @return
+ */
+int get_line_ex(void) {
+//    int c, i;
+//    extern char line[];
+//    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+//        line[i] = c;
+//    }
+//    if (c == '\n') {
+//        line[i] = c;
+//        ++i;
+//    }
+//    line[i] = '\0';
+//
+//    return i;
+}
+
+
+void copy_ex(void) {
+//    int i;
+//    i = 0;
+//    extern char line[], longest[];
+//    while ((longest[i] = line[i]) != '\0')
+//        ++i;
+}
+
+void longest_line_ex() {
+//    int len;
+//    int max;
+//    extern char longest[];
+//
+//    max = 0;
+//    while ((len = get_line_ex()) > 0) {
+//        if (len > max) {
+//            max = len;
+//            copy_ex();
+//        }
+//    }
+//
+//    if (max > 0) {
+//        printf("%s", longest);
+//    }
 }
